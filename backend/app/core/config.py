bdca967aa6d10b2b9c72e0edb1e8ab0fe15504d6
Kevin Cuadros <kev040798@gmail.com>
2025-12-21 20:16:31 -0500

    Create BE initial project

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Skill Coach Hub"
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
