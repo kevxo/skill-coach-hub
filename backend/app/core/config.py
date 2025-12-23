cf20c2c58b977a99e2ac3d8d0211fa8fa88f9e27
Kevin Cuadros <kev040798@gmail.com>
2025-12-22 21:51:34 -0500

    Setup configuration for jwt token

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Skill Coach Hub"
     DATABASE_URL: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ALGORITHM: str = "HS256"
 
     class Config:
         env_file = ".env"
