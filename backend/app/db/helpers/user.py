0b2ffe365b6a5a23ba046569a35946a80819a771
Kevin Cuadros <kev040798@gmail.com>
2025-12-26 21:44:52 -0500

    update user profile function created

--- backend/app/db/helpers/user.py
+++ backend/app/db/helpers/user.py
@@ -1,4 +1,5 @@
 from app.models.user import User
+from app.schemas.user import UserUpdate
 from sqlalchemy.orm import Session
 import uuid
 
@@ -14,3 +15,14 @@ def get_user_by_uuid(db: Session, uuid: uuid) -> User | Exception:
         )
 
     return user
+
+def update_user_profile(db: Session, payload: UserUpdate, current_user: User):
+    update_data = payload.model_dump(exclude_unset=True)
+
+    for field, value in update_data:
+        setattr(current_user, field, value)
+
+    db.commit()
+    db.refresh(current_user)
+
+    return current_user
