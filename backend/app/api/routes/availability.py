132594bf0aa350970c92c200f40ae80b5c8de216
Kevin Cuadros <kev040798@gmail.com>
2026-01-02 14:56:58 -0500

    Add get all and delete endpoints for availability

--- backend/app/api/routes/availability.py
+++ backend/app/api/routes/availability.py
@@ -6,7 +6,7 @@ from app.schemas.coach_availability import AvailabilityCreate, AvailabilityRespo
 from app.core.security import get_current_user
 from app.models.user import User
 from app.services.availability import has_overlay
-from app.db.helpers.availability import new_availability
+from app.db.helpers.availability import new_availability, all_my_availabilities, delete_my_availability
 
 router = APIRouter(prefix="/availability", tags=["availability"])
 
@@ -19,3 +19,18 @@ def create_availability(payload: AvailabilityCreate, db: Session = Depends(get_d
         )
 
     return new_availability(current_user, payload, db)
+
+@router.get("/me", response_model=list[AvailabilityResponse])
+def get_all_my_availabilities(
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(get_current_user)
+):
+    return all_my_availabilities(db, current_user)
+
+@router.delete("/{availability_id}", status_code=204)
+def delete_availability(
+    availability_id: str, 
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    delete_my_availability(availability_id, db, current_user)
--- backend/app/db/helpers/availability.py
+++ backend/app/db/helpers/availability.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.coach_availability import CoachAvailability
 from app.models.user import User
@@ -16,3 +17,32 @@ def new_availability(user: User, payload: AvailabilityCreate, db: Session):
     db.refresh(availability)
 
     return availability
+
+def all_my_availabilities(user: User, db: Session):
+    availabilities = (
+        db.query(CoachAvailability)
+        .filter(CoachAvailability.coach_id == user.id)
+        .order_by(
+            CoachAvailability.day_of_week,
+            CoachAvailability.start_time,
+        )
+        .all()
+    )
+
+    return availabilities
+
+def delete_my_availability(id: str, db: Session, user: User):
+    availability = (
+        db.query(CoachAvailability)
+        .filter(
+            CoachAvailability.id == id, 
+            CoachAvailability.coach_id == user.id
+        )
+        .first()
+    )
+
+    if not availability:
+        raise HTTPException(status_code=404, detail="Availability not found")
+
+    db.delete(availability)
+    db.commit()
